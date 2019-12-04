775e9752f2a788c884320f6e1e9f49ebcf8c3de6
Fig77 <55416179+Fig77@users.noreply.github.com>
2019-12-04 19:40:33 -0300

    refer to last commit

--- buble_sort.rb
+++ buble_sort.rb
@@ -1,34 +1,34 @@
 def bubble_sort(arr)
-		order = false
+  order = false
   until order do
-		order = true
-		for k in 0...arr.length-1
-			if arr[k] > arr[k+1]
-				arr[k], arr[k+1] = arr[k+1], arr[k]
-				order = false
-			end
-		end
-	end
+    order = true
+    for k in 0...arr.length-1
+      if arr[k] > arr[k+1]
+        arr[k], arr[k+1] = arr[k+1], arr[k]
+        order = false
+      end
+    end
+  end
 end
 
 def bubble_sort_by(array)
-	swap= false
-	while !swap
-		swap=true
-		i=0
-		loop do
-			if yield(array[i],array[i+1]) > 0
-				array[i], array[i+1] = array[i+1], array[i]
-				swap=false
-			end
-			i += 1
-			if i == (array.length-1)
-				break
-			end	
-		end
-	end
+  swap= false
+  while !swap
+    swap=true
+    i=0
+    loop do
+      if yield(array[i],array[i+1]) > 0
+        array[i], array[i+1] = array[i+1], array[i]
+        swap=false
+      end
+      i += 1
+      if i == (array.length-1)
+        break
+      end 
+    end
+  end
 end
 
 bubble_sort_by(["hi","hello","hey"]) do |left,right|
-	left.length - right.length
+  left.length - right.length
 end
